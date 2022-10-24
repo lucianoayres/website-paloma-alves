34fb3fbc1797e18f39f0c4d59bd2d55e80253b04
Luciano Ayres <lucianofarias@gmail.com>
2022-10-23 21:34:29 -0300

    feat: add light/dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,39 @@ function addCopyrightTextToFooter() {
 	footerElement.appendChild(pTag)
 }
 
+function switchTheme(theme) {
+	const currentSwitchThemeButton = getCurrentSwitchThemeButton()
+	const newSwitchThemeButton = getThemeSwitchButton(theme)
+
+	document.body.dataset.theme = theme
+
+	newSwitchThemeButton.style.display = 'none'
+	currentSwitchThemeButton.style.display = 'initial'
+}
+
+function getCurrentTheme() {
+	return document.body.dataset.theme
+}
+
+function getCurrentSwitchThemeButton() {
+	return getThemeSwitchButton(getCurrentTheme())
+}
+
+function getThemeSwitchButton(theme) {
+	return document.body.querySelector(`#${theme}-theme-switch`)
+}
+
+function hideElement(element) {
+	element.style.display = 'none'
+}
+
+function initTheme() {
+	const currentSwitchThemeButton = getCurrentSwitchThemeButton()
+	currentSwitchThemeButton.style.display = 'none'
+}
+
 function main() {
+	initTheme()
 	addCopyrightTextToFooter()
 }
 
